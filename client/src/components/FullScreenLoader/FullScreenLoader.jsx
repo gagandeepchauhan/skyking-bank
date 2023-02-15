bb7d2793caedea06cd9ea72bf8fe305767f834a1
gagandeepchauhan <gaganchauhanbe786@gmail.com>
2023-02-15 21:41:52 +0530

    modified pnf screen and loader

--- client/src/components/FullScreenLoader/FullScreenLoader.jsx
+++ client/src/components/FullScreenLoader/FullScreenLoader.jsx
@@ -4,8 +4,13 @@ import styles from './FullScreenLoader.module.css';
 const FullScreenLoader = () => {
     return (
         <div className={styles.loadContainer}>
-            <div className={styles.loadText}>
-                Loading...
+            <div className='text-center'>
+                <div className={`${styles.loader} spinner-border mb-2`} role="status">
+                    <span className="sr-only"></span>
+                </div>
+                <div className={styles.loadText}>
+                    Loading...
+                </div>
             </div>
         </div>
     )
--- client/src/screens/PageNotFoundScreen/PageNotFoundScreen.jsx
+++ client/src/screens/PageNotFoundScreen/PageNotFoundScreen.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './PageNotFoundScreenStyle.module.css';
 
 const PageNotFoundScreen = () => {
+    const navigate = useNavigate();
     return (
-        <div>PageNotFoundScreen</div>
+        <div className={`${styles.pnfScreen} container py-3 mt-5`}>
+            <div>
+                <div className={styles.title}>
+                    Page Not Found
+                </div>
+                <p className='light-text'>
+                    The page you are looking for either does not exist or you are not authenticated well
+                </p>
+                <button
+                    className='sky-btn'
+                    onClick={() => navigate('/')}
+                >
+                    go to valid page
+                </button>
+            </div>
+        </div>
     )
 };
 
